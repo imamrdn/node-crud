1adb768c5c1835e4228b7ada82805bb110b735e1
Imam Ramadhan <imamrmdn99@gmail.com>
2023-02-04 17:32:50 +0700

    update: move project

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.listen(3000, () => {
-    console.log('Server success running on port 3000')
+//routing
+//app.method(path, handler)
+app.use('/', (req, res, next) => {
+    res.send('Hello World')
+})
+
+app.listen(5000, () => {
+    console.log('Server success running on port 5000')
 })
