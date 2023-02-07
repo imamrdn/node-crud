ada42520498cdf4b3401d787015f008eece10532
Imam Ramadhan <imamrmdn99@gmail.com>
2023-02-07 22:03:51 +0700

    update: add database.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const usersRouters = require('./routes/users')
+const users = require('./routes/users')
 const middlewareLogRequest = require('./middlewares/logs')
 
 const app = express()
@@ -8,7 +8,22 @@ app.use(middlewareLogRequest)
 app.use(express.json())
 
 //app.method(path, handler)
-app.use('/users', usersRouters)
+app.use('/users', users)
+
+app.use('/', (req, res) => {
+    dbPool.execute('SELECT * FROM users', (err, rows) => {
+        if(err) {
+            res.json({
+                message: 'Connection failed'
+            })
+        }
+
+        res.json({
+            message: 'Connection success',
+            data: rows
+        })
+    })
+})
 
 app.listen(5000, () => {
     console.log("Server running on port 5000")
